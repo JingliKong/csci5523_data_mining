8afde2655487d2ccd82d40b0cb58ea88588b3e70
JingliKong <alpha282828@gmail.com>
2023-04-14 02:28:25 -0500

    GN alg really works serially now
    Still need to use spark to parallelize alg

--- hw4/graph_utils.py
+++ hw4/graph_utils.py
@@ -1,3 +1,5 @@
+import math
+import queue 
 class TreeNode:
     def __init__(self, name, height):
         self.name = name
@@ -16,7 +18,28 @@ class TreeNode:
         if len(self.parent) != 0:
             parents = [p.name for p in self.parent]
         
-        return f"node:{self.name}, children: {children}, parents: {parents}, score: {score}"
+        return f"node:{self.name}, children: {children}, parents: {parents}, level: {self.level}, score: {score}"
+
+
+def createGraph(nodes, edges):
+    '''
+    edges: [(0, 1), (0, 3), (0, 5), (0, 10), (0, 14), (0, 16), (0, 19), (0, 20), (0, 23), (0, 28)]
+    nodes: [0, 1, 3, 5, 10, 14, 16, 19, 20, 23]
+    '''
+
+    graph = {node: [] for node in nodes}
+
+    for node in nodes:
+        for edge in edges:
+            if node in edge:
+                other_node = edge[0] if edge[1] == node else edge[1]
+                graph[node].append(other_node)
+                
+    return graph
+
+test_nodes = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+test_edges = [('A', 'B'), ('A', 'C'), ('C', 'B'), ('D', 'B'), ('E', 'D'), ('F', 'D'), ('G', 'D'), ('F', 'E'), ('F', 'G')]
+print(createGraph(test_nodes, test_edges))
 
 # source: https://www.tutorialspoint.com/python_data_structure/python_graphs.htm
 # I am using how this article describes graphs in python to represent the graphs for my task2
@@ -35,7 +58,7 @@ class MyGraph:
         self.nodes = list(self.gdict.keys())
         # initializing our edges to a list
         self.edges = self.findEdges()
-        self.edgeDict = {e: 0 for e in self.findEdges()}
+        self.edgeDict = self.createEdgeDict()
     # gets the number of nodes aka vertices in the 
     
     def getVertices(self):
@@ -51,93 +74,70 @@ class MyGraph:
                     edges.append(candidate_edge)
         return edges
     def createEdgeDict(self):
-        self.edges = {e: 0 for e in self.findEdges()}
+        return {e: 0 for e in self.findEdges()}
     def createTree (self, start):
         '''
         Build a tree using TreeNode using bfs
         start: the starting node for our bfs 
         return: a list of all the TreeNodes created from bfs 
         '''
-        visited = []
-        name_of_visited_nodes = []
-        queue = [TreeNode(start, height=0)]
-        created_nodes: list[str] = [] 
-        # loop until we have visited all the nodes
-        while len(queue) != 0: # FIXME: Include a maxiter incase we have a disconnected graph
-            # the node we are exploring
-            node = queue.pop(0)
-            # getting the name
-            name = node.name
-            level = node.level
-            # checking if we have visited this node before
-            if node.name not in name_of_visited_nodes:
-                # adding the current node to a list maintaining all the nodes we have visited
-                name_of_visited_nodes.append(name)
-                # getting all neighbors and adding them to queue
-                neighbors = []
-                for n in self.gdict[node.name]:
-                    if (n not in name_of_visited_nodes):
-                        neighbors.append(n)
-                # for each child
-                for neighbor in neighbors:
-                    # we add all nodes that this node connect to as children for our bfs tree
-                    node.children.add(neighbor) 
-                    if neighbor not in created_nodes:
-                        # we allocate the current neighbor and set its level 
-                        neighbor_node = TreeNode(neighbor, level+1)
-                        # each of these newly allocated TreeNodes have the current explored node as their parent
-                        neighbor_node.parent.add(node)
-                        # appending the current node to the queue
-                        queue.append(neighbor_node)
-                        # keep track of variables allocated
-                        created_nodes.append(neighbor)
-                    else: # node already is in queue so we have to edit it's parent to append the current node
-                        for i in range(len(queue)):
-                            if queue[i].name == neighbor:
-                                queue[i].parent.add(node)
-                
-                # at this point we have to add the node to our visited list 
-                visited.append(node) # node is a TreeNode
-        # sets all the leaf node's children to be nothing because they are leaves
-        total_height = visited[-1].level
-        for n in visited:
-            node_parents = n.parent.copy()  # create a copy of the set
-            for p in node_parents:
-                if p.level == n.level:
-                    n.parent.remove(p)  # modify the original set
-
-            if (n.level == total_height):
-                n.children = set()
-        # removing extra parents just because a node is a neighbor doesn't mean that its a parent
-
-        return visited 
-def calcWeightsForGN(graph: MyGraph, queue: list[TreeNode]) -> dict[frozenset]:
+        nodes = []
+        bfs_results = []
+        visited_nodes: list[str] = [] # names of the nodes that we have visited 
+        # first initialize all the TreeNodes we'll need for this tree 
+        for n in self.nodes:
+            nodes.append(TreeNode(n,math.inf))
+        # we have to find our starting node
+        root = None
+        for n in nodes:
+            if n.name == start:
+                root = n
+                n.level = 0
+                break
+        # create a queue starting at the starting node which is the root
+        assert (root != None)
+        myQueue = queue.Queue()
+        myQueue.put(root)
+        # iterate until the queue is empty
+        while (myQueue.qsize() != 0): # TODO add max iter
+            current_node = myQueue.get()
+            neighbors = self.gdict[current_node.name] # list of nodes that are neighbors
+            # look through allocated nodes and see which ones are our neighbors
+            for n in neighbors:
+                for node in nodes:
+                    if node.name == n and node.level > current_node.level:
+                        node.level = current_node.level + 1
+                        node.parent.add(current_node)  # setting the parent as a ref to the current node
+                        current_node.children.add(node) 
+                        if (node.name not in visited_nodes):
+                            myQueue.put(node)
+                            visited_nodes.append(node.name)
+            bfs_results.append(current_node)
+        return bfs_results                        
+            
+def calcWeightsForGN(graph: MyGraph, tree: list[TreeNode]) -> dict[frozenset]:
     '''assigns the node weights and edge weights for the Girvan-Newman alg'''
     edge_betweeness = {edge: 0 for edge in graph.edges}
-    # to store current nodes we are exploring we start exploring one of the leaf nodes 
+    # we need to start at highest level and recurse from max height back to 0
+    starting_level = tree[len(tree)-1].level # we start from the leaf nodes onward to calculate betweeness
+    levels_of_tree = [] # a list to hold the number of nodes per level index 3 contains how many nodes at height 3
+    while (starting_level >= 0):
+        per_level = []
+        for node in tree:
+            if node.level == starting_level:
+                per_level.append(node)
+        levels_of_tree.append(per_level)
+        starting_level -= 1
+    for level in levels_of_tree:   
+        for node in level:
+            credit = 0
+            if (len(node.parent) != 0):
+                credit = node.score / len(node.parent)
+            for p in node.parent:
+                edge = frozenset((node.name,p.name))
+                edge_betweeness[edge] += credit
+                p.score += credit
 
-    # calculating edge weights and the weights of all nodes
-    # recall that the queue starts off with the leaf nodes
-    while(len(queue) > 0): 
-        # recall: A DAG edge e entering node Z from the level above is given a share of the
-        # credit of Z proportional to the fraction of shortest paths from the root to Z that go through pg 12 in chap 10 mmds
-        current_node = queue.pop()
-        # a literally reference to parent node addresses
-        parents = []
-        for p in current_node.parent: # p is a reference to another node which is a parent
-            parents.append(p)
-        if len(parents) != 0:
-            credit_share = current_node.score / len(parents)
-        else:
-            credit_share = 0
-        # holds all the edges from current node to parent(s)
-        for p in parents: 
-            # creates edges from our current node so we know which edges to give credit to
-            current_edge = frozenset((current_node.name, p.name))
-            # adding our credit to the edge
-            edge_betweeness[current_edge] += credit_share
-            # we also increase our parent node's score based on the credit we give them
-            p.score += credit_share
 
     return edge_betweeness
 
@@ -165,9 +165,9 @@ def calcWeightsForGN(graph: MyGraph, queue: list[TreeNode]) -> dict[frozenset]:
 
 graph_elements = { 
    "A" : ["B","C"],
-   "B" : ["A", "C"],
+   "B" : ["A", "C", "D"],
    "C" : ["A", "B"],
-   "D" : ["B", "G"],
+   "D" : ["B", "E", "F", "G"],
    "E" : ["D", "F"],
    "F" : ["D", "E", "G"],
    "G" : ["D", "F"]
@@ -175,28 +175,33 @@ graph_elements = {
 
 # create a graph using graph elements 
 g = MyGraph (graph_elements)
-
+print(g.nodes)
+print([tuple(item) for item in g.edges])
 # !!!!!!!!EACH PROC NEEDS ITS OWN GRAPH MY DUDE
 
 # create a tree from my graph from a particular node
 tree = g.createTree("E")
-for t in tree:
-    print(t)
+# for t in tree:
+#     print(t)
 # for the tree calculate all the edges and weights for nodes
 # everything works we guchi
 # betweeness = calcWeightsForGN(g, tree)  # {frozenset({'A', 'B'}): 1.0, frozenset({'A', 'C'}): 0, frozenset({'B', 'C'}): 1.0, frozenset({'B', 'D'}): 3.0, frozenset({'G', 'D'}): 0.5, frozenset({'E', 'D'}): 4.5, frozenset({'E', 'F'}): 1.5, frozenset({'F', 'D'}): 0, frozenset({'F', 'G'}): 0.5}
 
 # testing if we can calculate the betweeness for the entire graph
 # nodes that we bfs we will eventually use them to calculate betweeness 
-# nodes = []
+# trees = []
 # for k, v in graph_elements.items():
-#     nodes.append(g.createTree(k))
+#     trees.append(g.createTree(k))
 
 # all_betweenesses: list[dict] = []
-# for n in nodes:
-#     all_betweenesses.append(calcWeightsForGN(g, n))
+# for tree in trees:
+#     all_betweenesses.append(calcWeightsForGN(g, tree))
 
 # sum_betweeness = g.createEdgeDict()
-# # for b in all_betweenesses:
+# for b in all_betweenesses:
+#     for k, v in b.items():
+#         sum_betweeness[k] += v
+# sum_betweeness = {k: v/2 for k, v in sum_betweeness.items()} 
+# print(sum_betweeness)
 
-print()
+# yo this works lets goo
